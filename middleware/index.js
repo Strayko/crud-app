6074d804a107e5f6d017c46b4b4e1d33897e033a
Strayko <strayko_lol@windowslive.com>
2018-10-21 13:17:39 +0200

    refactoring middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,5 @@
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
-
 var middlewareObj = {};
 
 middlewareObj.checkCampgroundOwnership = function(req, res, next) {
