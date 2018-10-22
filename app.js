13b9344f8f0a80416b957d5d770529465038edab
Strayko <strayko_lol@windowslive.com>
2018-10-22 18:13:00 +0200

    user profile page

--- app.js
+++ app.js
@@ -47,7 +47,9 @@ app.use(function(req, res, next) {
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-
+app.use("/profile", function(req, res) {
+    res.render("profile/profile");
+});
 // app.listen(process.env.PORT, process.env.IP, function () {
 //     console.log("Server has started");
 // });
